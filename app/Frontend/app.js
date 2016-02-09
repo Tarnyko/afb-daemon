3d481682031d18134b6ecfcb914a9683b8d5fd3e
Fulup Ar Foll <fulup@iot.bzh>
2016-02-09 18:48:09 +0100

    Implemented URL query parsing for initial token /opa/?token=abcde

--- app/Frontend/app.js
+++ app/Frontend/app.js
@@ -1,13 +1,13 @@
 (function() {
   'use strict';
   
-  function ParseQueryString () {
+  function  ParseQueryString () {
       var qd = {};
       location.search.substr(1).split("&").forEach(function(item) {
           var k = item.split("=")[0], v = decodeURIComponent(item.split("=")[1]); (k in qd) ? qd[k].push(v) : qd[k] = [v];
       });
       return qd;
-  }
+  };
 
   angular.module('@@APPNAME@@', [ // Warning: Appname should fit with gulpfile.js & index.html
     'ui.router',
